60870d5cf6542779b1438fddb50e4d61ee576c13
mikakane <t.goto@chatbox-inc.com>
2016-11-08 17:06:46 +0900

    Update Cart.php

--- app/Service/Cart.php
+++ app/Service/Cart.php
@@ -7,16 +7,37 @@ use Illuminate\Support\Facades\DB;
 
 /*
  * カート周りのメソド
+ * カートのデータは session items キーに登録する
+ * items の中身の形式で
+ * [
+ *    $id => [
+ *        item => $itemData
+ *        amount => 2
+ *    ],
+ * ]
  *
  * */
 class Cart extends Model
 {
-    //商品の追加
+    /**
+     * 商品の追加。
+     * 追加時点で商品をまとめて集計する
+     */
     public function addItem($id, $amount){
+        
         $item = DB::table('vegetables')->where('id', $id)->first(); //idが一致するものをvegetableテーブルから検索、取得
-        $item->amount = $amount; //objの$itemにamountを追加
+        
         $items = session()->get("items",[]); //セッションデータを取得、nullの場合は空の配列
-        $items[] = $item; // 取得したデータにオブジェクトを保存
+        if(isset($item[$id]))
+        {//登録済み商品の追加パターン
+            $item[$id]["amount"] = $item[$id]["amount"]+$amouont;
+        }else
+        {//新規追加アイテムパターン
+            $item[$id] = [
+                "item" => $item,
+                "amouont" => $amount
+            ]
+        }        
         session()->put("items", $items); //取得したデータをsessionに保存。 $_SESSION["items"] に保存するのと同じ
     }
     //商品の削除
